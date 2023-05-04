f4e7f1d0d158adf46e84d52969cdeb0cbbdc20a6
Ashenafi-Tesfaye <65870089+Ashenafi-Tesfaye@users.noreply.github.com>
2023-05-04 19:01:49 -0400

    test2

--- AGrespapidemo/src/main/java/com/AGrespapi/AGrespapidemo/service/UserDetailServiceImpl.java
+++ AGrespapidemo/src/main/java/com/AGrespapi/AGrespapidemo/service/UserDetailServiceImpl.java
@@ -19,7 +19,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-		System.out.println("testing hello world");
+		System.out.println("testing hello world. This is for test.");
 		User currentUser = repository.findByUsername(username);
 		 UserDetails user = new org.springframework.security.core.userdetails.User(username, currentUser.getPassword(),
 				 true,true,true,true, AuthorityUtils.createAuthorityList(currentUser.getRole()));
